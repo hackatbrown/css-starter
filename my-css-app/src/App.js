0f17d3f9e9b019a7be838b400c8f162aa03f7648
rjiang6 <renajiang@gmail.com>
2023-01-27 12:29:47 -0500

    updated readme & cssapp

--- my-css-app/src/App.js
+++ my-css-app/src/App.js
@@ -2,85 +2,91 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <h1 className="h1">
-            Flex container with flex direction "row" (note that we apply the properties for this text using the "h1" selector)
-        </h1>
-        <div className="flex-container-row">
-            <div className="flex-item">
-                Item 1
+    return (
+        <div className="App">
+            <div className="header">
+                <h1>
+                    Flex container with flex direction "row" (note that we apply the properties for this text using the "h1" selector)
+                </h1>
             </div>
-            <div className="flex-item">
-                Item 2
+            <div className="flex-container-row">
+                <div className="flex-item">
+                    Item 1
+                </div>
+                <div className="flex-item">
+                    Item 2
+                </div>
+                <div className="flex-item">
+                    Item 3
+                </div>
+                <div className="flex-item">
+                    Item 4
+                </div>
+                <div className="flex-item">
+                    Item 5
+                </div>
+                <div className="flex-item">
+                    Item 6
+                </div>
             </div>
-            <div className="flex-item">
-                Item 3
+            <div className="header">
+                <h2>
+                    Flex container with flex direction "column" (note that we apply the properties for this text using the "h2" selector)
+                </h2>
             </div>
-            <div className="flex-item">
-                Item 4
+            <div className="flex-container-col">
+                <div className="flex-item">
+                    Item 1
+                </div>
+                <div className="flex-item">
+                    Item 2
+                </div>
+                <div className="flex-item">
+                    Item 3
+                </div>
             </div>
-            <div className="flex-item">
-                Item 5
+            <div className="header">
+                <h3>
+                    You can also nest flex containers!  (note that we apply the properties for this text using the "h3" selector)
+                </h3>
             </div>
-            <div className="flex-item">
-                Item 6
+            <div className="flex-container-row">
+                <div className="flex-container-col">
+                    <div className="flex-item">
+                        Item 1
+                    </div>
+                    <div className="flex-item">
+                        Item 2
+                    </div>
+                    <div className="flex-item">
+                        Item 3
+                    </div>
+                </div>
+                <div className="flex-container-col">
+                    <div className="flex-item">
+                        Item 4
+                    </div>
+                    <div className="flex-item">
+                        Item 5
+                    </div>
+                    <div className="flex-item">
+                        Item 6
+                    </div>
+                </div>
+                <div className="flex-container-col">
+                    <div className="flex-item">
+                        Item 7
+                    </div>
+                    <div className="flex-item">
+                        Item 8
+                    </div>
+                    <div className="flex-item">
+                        Item 9
+                    </div>
+                </div>
             </div>
         </div>
-        <h2 className="h2">
-          Flex container with flex direction "column" (note that we apply the properties for this text using the "h2" selector)
-        </h2>
-        <div className="flex-container-col">
-            <div className="flex-item">
-                Item 1
-            </div>
-            <div className="flex-item">
-                Item 2
-            </div>
-            <div className="flex-item">
-                Item 3
-            </div>
-        </div>
-        <h3 className="h3">
-          You can also nest flex containers!  (note that we apply the properties for this text using the "h3" selector)
-        </h3>
-        <div className="flex-container-row">
-        <div className="flex-container-col">
-            <div className="flex-item">
-                Item 1
-            </div>
-            <div className="flex-item">
-                Item 2
-            </div>
-            <div className="flex-item">
-                Item 3
-            </div>
-        </div>
-        <div className="flex-container-col">
-            <div className="flex-item">
-                Item 4
-            </div>
-            <div className="flex-item">
-                Item 5
-            </div>
-            <div className="flex-item">
-                Item 6
-            </div>
-        </div>
-        <div className="flex-container-col">
-            <div className="flex-item">
-                Item 7
-            </div>
-            <div className="flex-item">
-                Item 8
-            </div>
-            <div className="flex-item">
-                Item 9
-            </div>
-        </div>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default App;
